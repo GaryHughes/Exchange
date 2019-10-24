fa05ab5dd0b05dad09785901480090c1d90093a8
Matt Curtis <matt.r.curtis@gmail.com>
2019-10-23 20:33:42 -0700

    Rust: use buffered output to avoid locking
    This gives a 12% speedup on my machine (8.7s to 7.6s, x86_64 rustc 1.37.0)
    
    ./target/release/exchange > ../rust-trades-10M.txt
    6.68s user 0.77s system 97% cpu 7.674 total
    6.77s user 0.80s system 99% cpu 7.591 total
    6.75s user 0.78s system 99% cpu 7.554 total
    6.77s user 0.80s system 99% cpu 7.585 total
    6.76s user 0.79s system 99% cpu 7.564 total

--- Part 2/rust_solution/src/main.rs
+++ Part 2/rust_solution/src/main.rs
@@ -1,6 +1,7 @@
 use csv::{ReaderBuilder, StringRecord};
 use std::error::Error;
 use std::io;
+use std::io::{Write, BufWriter};
 use std::process;
 
 extern crate exchange;
@@ -15,13 +16,15 @@ pub fn run() -> Result<(), Box<dyn Error>> {
         .from_reader(io::stdin());
     let mut record = StringRecord::with_capacity(100, 4);
     let mut gen = 0;
+    let stdout = io::stdout();
+    let mut buf_stdout = BufWriter::new(stdout);
     while rdr.read_record(&mut record)? {
         gen += 1;
         let instrument = &record[1];
         let order = order::read(&record[0], &record[2], &record[3], gen)?;
         let trades = exchange.execute(instrument, order);
         for t in trades {
-            println!("{}", t);
+            writeln!(buf_stdout, "{}", t)?;
         }
     }
     Ok(())
