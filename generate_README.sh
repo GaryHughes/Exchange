6382b32ff62c2993778d2da2ed3a8a8ec1a0b89e
Gary Hughes <garyedwardhughes@gmail.com>
2019-11-03 10:10:10 +1100

    format fix.

--- generate_README.sh
+++ generate_README.sh
@@ -3,11 +3,11 @@
 cp README-header.md README.md
 
 if [ -f part_1_results.md ]; then
-    echo "##Part 1 Performance" >> README.md
+    echo "## Part 1 Performance" >> README.md
     cat part_1_results.md >> README.md
 fi
 
 if [ -f part_2_results.md ]; then
-    echo "##Part 2 Performance" >> README.md
+    echo "## Part 2 Performance" >> README.md
     cat part_2_results.md >> README.md
 fi
