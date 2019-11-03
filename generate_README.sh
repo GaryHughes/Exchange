b2458c795e499173aac153b1c8e0d973fb25161d
Gary Hughes <garyedwardhughes@gmail.com>
2019-11-03 15:35:58 +1100

    Add build badges for the benchmarks.

--- generate_README.sh
+++ generate_README.sh
@@ -3,11 +3,11 @@
 cp README-header.md README.md
 
 if [ -f part_1_results.md ]; then
-    echo "## Part 1 Performance" >> README.md
+    cat README_part_1_header.md >> README.md
     cat part_1_results.md >> README.md
 fi
 
 if [ -f part_2_results.md ]; then
-    echo "## Part 2 Performance" >> README.md
+    cat README_part_2_header.md >> README.md
     cat part_2_results.md >> README.md
 fi
