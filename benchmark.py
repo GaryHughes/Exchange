26edf82097c8525b50d87a433567451f9976835c
Gary Hughes <garyedwardhughes@gmail.com>
2020-05-21 19:44:25 +1000

    Exclude swift and python from the large file.

--- benchmark.py
+++ benchmark.py
@@ -70,7 +70,13 @@ for input in input_files:
             os.chmod(solution_file, 0o755)
         command = "subprocess.run(['./runner < {} > {}'], shell=True, cwd='{}')".format(input_file, output_file, working_directory)
         try:
+            # TODO - support an exlusion file so we don't hard code this.
             if solution.find('python') >= 0 or solution.find('swift') >= 0:
+                # These solutions are too slow for the big file and relatively slow on
+                # the small file. Because the comparison with other solutions isn't as important
+                # we don't care so much about the validity of the results.
+                if order_count > 100000:
+                    continue
                 actual_iterations = 1
             else:
                 actual_iterations = iterations
