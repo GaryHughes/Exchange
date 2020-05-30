9724db294d7f2204bd4ceb962b82781ceaa83a67
Gary Hughes <garyedwardhughes@gmail.com>
2020-05-30 11:53:21 +1000

    Add C builds.

--- benchmark.py
+++ benchmark.py
@@ -104,7 +104,8 @@ languages = {
     "swift"     : "Swift",
     "java"      : "Java",
     "python"    : "Python",
-    "cython"    : "Cython"
+    "cython"    : "Cython",
+    "c"         : "C"
 }
 
 def language(solution):
